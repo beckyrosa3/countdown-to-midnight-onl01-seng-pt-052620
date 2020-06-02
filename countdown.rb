eae664aa539b93f605a617450e360c749086393f
Becky Rosa <becky_m_rosa@yahoo.com>
2020-06-02 03:33:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,9 @@ x = 1
   sleep(1.0)
   return "HAPPY NEW YEAR!"
 
-def countdown_with_sleep
+end
+
+def countdown_with_sleep(countdown)
   time = Time.now
   sleep 5.seconds until Time.now == 0
   end
