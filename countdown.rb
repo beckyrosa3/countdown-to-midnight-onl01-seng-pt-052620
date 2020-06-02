2fc10597a5ae4e6da205dbbcf7c69fab5e9a4ddd
Becky Rosa <becky_m_rosa@yahoo.com>
2020-06-02 03:38:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 def countdown
-
 x = 10
   while x > 0
     puts "#{x} SECOND(S)!"
   x +- 1
   return "HAPPY NEW YEAR!"
-
+  if counter == 0
+    break
+  end
 end
 
 def countdown_with_sleep(countdown)
